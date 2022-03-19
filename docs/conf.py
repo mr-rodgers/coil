a61460403b6fde838113ed297327fe4cea89c70a
Te-jé Rodgers <tjd.rodgers@gmail.com>
2022-03-19 18:59:34 +0100

    chore: add spacing to signatures in docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/autodoc.css"]
 
 # Intersphinx options
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
